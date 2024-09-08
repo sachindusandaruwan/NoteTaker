3c5abad6309052829db338cd0377a13610f4f8b3
sachindusandaruwan <sadaruwansachindu@gmail.com>
2024-09-08 14:46:22 +0530

    add userNotFoundException class

---
+++ src/main/java/lk/ijse/gdse/aad68/notetaker/exception/UserNotFoundException.java
@@ -0,0 +1,8 @@
+package lk.ijse.gdse.aad68.notetaker.exception;
+
+public class UserNotFoundException extends RuntimeException {
+  public UserNotFoundException() {}
+  public UserNotFoundException(String message) {}
+  public UserNotFoundException(String message, Throwable cause) {}
+
+}
